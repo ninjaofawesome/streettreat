c33a778567b7ae41ca851a2e12590b0737e305c9
Hannah Nordgren <hey@hereshannahs.info>
2015-09-29 08:17:58 -0400

    started in on tabulating results

--- js/logic.js
+++ js/logic.js
@@ -1,6 +1,6 @@
-//options: hotdog, falafel, street meat, pretzel, tacos, ice cream, fruit, waffel, nuts
+//options: hotdog, falafel, street meat, pretzel, tacos, ice cream, fruit, waffel, nuts, coffee/danish or egg and cheese on a roll
 
-//carts: hotdog, streetmeat, tacos, ice cream, fruit, waffel, nut cart
+//carts: hotdog, streetmeat, tacos, ice cream, fruit, waffel, nut cart, coffee cart
 
 //write a function to determine if the user is vegetarian, vegan, omnivore or flexitarian (boolean)
 function eatPrefs(){
@@ -8,62 +8,80 @@ function eatPrefs(){
   var prefButton = eatButtons.id;
   $(thisButton).click(function(){
     var likes = this;
-    var anything;
     if(likes === 'veggie'){
       console.log('Vegetarian');
-      anything = false;
+      var anything = false;
     }
     if(likes === 'vegan'){
       console.log('Vegan');
-      anything = false;
+      var anything = false;
     }
     if(likes === 'omni'){
       console.log('Omnivore');
-      anything = true;
+      var anything = true;
     }
     if(likes === 'flex'){
       console.log('Flexitarian');
-      anything = true;
+      var anything = true;
     }
   });
+  return anything;
 }
+
+var prefs = eatPrefs();
 //write a function to determine if the user wants savory or sweet (boolean)
 function tasteType(){
   var tasteButtons = document.getElementsByClassName('taste-type');
   var yumButton = tasteButtons.id;
   $(yumbutton).click(function(){
     var taste = this;
-    var craving;
     if(taste == 'sweet'){
       console.log('Sweet Treat!');
-      craving = false;
+      var craving = false;
     }
     if(taste == 'savory'){
       console.log('Savory Treat!');
-      craving = true;
+      var craving = true;
     }
   })
-
+  return craving;
 }
+
+var tasty = tasteType();
 //write a function to determine hunger level?
 function howHungry(){
   var hungerGames = document.getElementsByClassName('hunger-games');
   var portionButton = hungerGames.id;
   $(portionButton).click(function(){
     var level = this;
-    var starving;
     if(level == 0){
       console.log('snacktime');
-      starving = false;
+      var starving = false;
     }
     if(level == 1){
       console.log('meal');
-      starving = true;
+      var starving = true;
     }
   })
+  return starving;
 }
 
+var hangry = howHungry();
+
 //write a function to tally results of trues and falses
+
+function sweetOrNah(ep, hl, c){
+  var eatType = ep,
+      hunger = hl,
+      crave = c;
+
+  var result = [];
+  //add results, determine likelyhood of flex, omni, veg, vegan
+  //use mustache to template results.
+
+}
+
+sweetOrNah(prefs, tasty, hangry);
   // food results
   // if vegan, sweet, snack = true, fruit
   // if vegan, savory, snack = true, pretzel
@@ -78,11 +96,11 @@ function howHungry(){
   // if omni, sweet, snack = true, ice cream
   // if omni, savory, snack = true, hot dog
   // if omni, sweet, meal = true, ice cream, nuts and fruit
-  // if omni, savory, meal = true, street meat and ice cream
+  // if omni, savory, meal = true, street meat and fruit
 
   // if flex, sweet, snack = true, nuts
   // if flex, savory, snack = true, hot dog
   // if flex, sweet, meal = true, waffel and hot dog
-  // if flex, savory, meal = true, street meat and fruit
+  // if flex, savory, meal = true, egg and cheese or street meat
 
 //write a function to display street treat
